f88e6ba4ce98f4cc6759136fc9cbf29db75f6082
Ruth Chisom <ndukwe100805043@gmail.com>
2020-04-14 19:17:33 +0100

    Update estimator.js

--- src/estimator.js
+++ src/estimator.js
@@ -2,32 +2,31 @@ const covid19ImpactEstimator = (data) => data;
 // var reportedCases;
 const impact = {};
 const severeImpact = {};
-const timeToElapse = {};
-const data = covid19ImpactEstimator['data'];
+// const timeToElapse = {};
+const data1 = covid19ImpactEstimator.data;
 
-function calcDays(data) {
-  if (data.periodType === 'days') timeToElapse = data.timeToElapse;
-  else if (data.periodType === 'weeks') timeToElapse = data.timeToElapse * 7;
-  else if (data.periodType === 'months') timeToElapse = data.timeToElapse * 30;
-  const ratio = Math.floor(timeToElapse / 3);
+function calcDays(data1) {
+  if (data1.periodType === 'days') timeToElapse = data1.timeToElapse;
+  else if (data1.periodType === 'weeks') timeToElapse = data1.timeToElapse * 7;
+  else if (data1.periodType === 'months') timeToElapse = data1.timeToElapse * 30;
+  ratio = Math.floor(timeToElapse / 3);
   return ratio;
 }
 
-calcDays(data);
+calcDays(data1);
 
-function calcImpact(data, ratio) {
-  impact.currentlyInfected = Math.floor(data.reportedCases * 10);
+function calcImpact(data1, ratio) {
+  impact.currentlyInfected = Math.floor(data1.reportedCases * 10);
   impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** ratio);
   return impact;
 }
 
-function calcSevereImpact(data, ratio) {
-  severeImpact.currentlyInfected = Math.floor(data.reportedCases * 10);
+function calcSevereImpact(data1, ratio) {
+  severeImpact.currentlyInfected = Math.floor(data1.reportedCases * 50);
   severeImpact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** ratio);
   return severeImpact;
 }
 
-severeImpact.currentlyInfected = Math.floor(reportedCases * 50);
-calcImpact(data,ratio);
-calcSevereImpact(data,ratio);
+calcImpact(data1,ratio);
+calcSevereImpact(data1,ratio);
 export default covid19ImpactEstimator;
