7c5582d09b81b4b650309f828748c0dddcc5da93
Ruth Chisom <ndukwe100805043@gmail.com>
2020-04-14 18:40:24 +0100

    Challenge 1

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,31 @@
 const covid19ImpactEstimator = (data) => data;
+// var reportedCases;
+var impact = {};
+var severeImpact = {};
 
+function calcDays(){
+    if(data.periodType=="days") timeToElapse = data.timeToElapse;
+    else if(data.periodType=="weeks") timeToElapse = data.timeToElapse * 7;
+    else if(data.periodType=="months") timeToElapse = data.timeToElapse * 30;
+    var ratio = math.floor(timeToElapse/3);
+    return ratio;
+}
+
+calcDays();
+
+function calcImpact() {
+    impact.currentlyInfected = Math.floor(data.reportedCases * 10);
+    impact.infectionsByRequestedTime = impact.currentlyInfected * Math.pow(2,ratio);
+    return impact;
+}
+
+function calcSevereImpact(){
+    severeImpact.currentlyInfected = Math.floor(data.reportedCases * 10);
+    severeImpact.infectionsByRequestedTime = impact.currentlyInfected * Math.pow(2,ratio);
+    return severeImpact;
+}
+
+severeImpact. currentlyInfected = Math.floor(reportedCases * 50);
+calcImpact();
+calcSevereImpact();
 export default covid19ImpactEstimator;
