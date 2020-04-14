fe8dec44fae054e080e20358ff755fcc70854fd4
Ruth Chisom <ndukwe100805043@gmail.com>
2020-04-14 19:06:05 +0100

    Update estimator.js

--- src/estimator.js
+++ src/estimator.js
@@ -1,32 +1,33 @@
 const covid19ImpactEstimator = (data) => data;
 // var reportedCases;
-let impact = {};
-let severeImpact = {};
-let timeToElapse;
+const impact = {};
+const severeImpact = {};
+const timeToElapse;
+const data = covid19ImpactEstimator['data'];
 
-function calcDays (data){
-    if (data.periodType === 'days') timeToElapse = data.timeToElapse;
-    else if (data.periodType === 'weeks') timeToElapse = data.timeToElapse * 7;
-    else if (data.periodType === 'months') timeToElapse = data.timeToElapse * 30;
-    let ratio = math.floor(timeToElapse/3);
-    return ratio;
+function calcDays(data) {
+  if (data.periodType === 'days') timeToElapse = data.timeToElapse;
+  else if (data.periodType === 'weeks') timeToElapse = data.timeToElapse * 7;
+  else if (data.periodType === 'months') timeToElapse = data.timeToElapse * 30;
+  const ratio = Math.floor(timeToElapse / 3);
+  return ratio;
 }
 
 calcDays(data);
 
-function calcImpact(data,ratio) {
-    impact.currentlyInfected = Math.floor(data.reportedCases * 10);
-    impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** ratio);
-    return impact;
+function calcImpact(data, ratio) {
+  impact.currentlyInfected = Math.floor(data.reportedCases * 10);
+  impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** ratio);
+  return impact;
 }
 
-function calcSevereImpact(data,ratio){
-    severeImpact.currentlyInfected = Math.floor(data.reportedCases * 10);
-    severeImpact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** ratio);
-    return severeImpact;
+function calcSevereImpact(data, ratio) {
+  severeImpact.currentlyInfected = Math.floor(data.reportedCases * 10);
+  severeImpact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** ratio);
+  return severeImpact;
 }
 
-severeImpact. currentlyInfected = Math.floor(reportedCases * 50);
+severeImpact.currentlyInfected = Math.floor(reportedCases * 50);
 calcImpact(data,ratio);
 calcSevereImpact(data,ratio);
 export default covid19ImpactEstimator;
